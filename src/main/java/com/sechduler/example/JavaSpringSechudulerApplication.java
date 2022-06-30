f630c37db851fa74f433c9785643eb90109cb858
Ramesh <rameshchandrapanda15@gmail.com>
2022-06-30 13:16:36 +0530

    first commit

---
+++ src/main/java/com/sechduler/example/JavaSpringSechudulerApplication.java
@@ -0,0 +1,13 @@
+package com.sechduler.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringSechudulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringSechudulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sechduler/example/JavaSpringSechudulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sechduler.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringSechudulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
